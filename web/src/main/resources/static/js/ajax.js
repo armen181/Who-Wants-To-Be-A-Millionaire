516427269222eca97d111adf817d21f831ae166b
Armen <Armen.181@gmail.com>
2018-09-02 23:43:33 +0400

    v1.0.01

---
+++ web/src/main/resources/static/js/ajax.js
@@ -0,0 +1,26 @@
+ $(document).ready(function () {
+
+    $("#beginGame").click(function () {
+            //var bla = $('#inputButtonPaper').val();
+           // var radioButton = Number($('[name="optradio"]:checked').closest('label').text());
+            var settings = {
+                "async": true,
+                "crossDomain": true,
+                "url": "getQuestion",
+                "method": "GET",
+                "headers": {
+                    "guess": bla,
+                    "radioButton": radioButton,
+                }
+            }
+
+            $.ajax(settings).done(function (response) {
+
+               // $('#result').text(response.message);
+            });
+        });
+
+
+
+
+    });
