dba9641dd9d64fb951b70b63ff3de79d9d3249e9
Armen <Armen.181@gmail.com>
2018-09-03 14:19:34 +0400

    v1.0.04

--- web/src/main/java/armen/springbootmillionaires/controller/GameController.java
+++ web/src/main/java/armen/springbootmillionaires/controller/GameController.java
@@ -3,12 +3,15 @@ package armen.springbootmillionaires.controller;
 
 
 
+import armen.springbootmillionaires.DB.Questions;
 import armen.springbootmillionaires.service.GameService;
 import armen.springbootmillionaires.service.MessageService;
 import armen.springbootmillionaires.util.QuestionArray;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -16,18 +19,66 @@ import org.springframework.web.bind.annotation.RestController;
 public class GameController {
 
 
+  // == field ==
+
+  public static final String START_MESSAGE = "start.msg";
+  public static final String GAME_DESKRP = "game.deskrp";
+  public static final String RIGHT_ANSWER = "right.answer";
+  public static final String WRONG_ANSWER = "wrong.answer";
+  public static final String WIN_MONEY = "win.money";
+  public static final String LOSE_GAME = "lose.game";
+  public static final String GAME_OVER= "game.over";
+  public static final String PLAY_AGAIN = "play.again";
+
   private GameService gameService;
 
+  private QuestionArray questionArray;
+
   private MessageService messageService;
+
+
+  private boolean status = false;
+  private boolean use50_50 = false;
+  private Questions newQuestion;
+
+  // == constructor ==
+
   @Autowired
-  public GameController(GameService gameService, MessageService messageService) {
+  public GameController(GameService gameService, QuestionArray questionArray, MessageService messageService) {
     this.gameService = gameService;
+    this.questionArray = questionArray;
     this.messageService = messageService;
   }
 
+
+  // === RQ ===
   @GetMapping("startGame")
-    public String getQuestion(){
-      return "Hello";
+    public Questions getQuestion(){
+    for(Questions questions : questionArray.getQuestionsArray()) {
+      gameService.addQuestion(questions);
+    }
+    gameService.initOrReset();
+    status = false;
+    use50_50 = false;
+    newQuestion = messageService.getNextMessage();
+    return newQuestion;
+
+  }
+
+  @PostMapping("answer")
+  public Questions chackAnswer(@RequestHeader int answer) {
+    int status = messageService.getResultMessage(newQuestion, answer);
+
+    if (status == 1) {
+      newQuestion.setQuestionId("true");
+      return newQuestion;
+    } else if (status == 0) {
+      newQuestion= gameService.make50_50(newQuestion);
+      return newQuestion;
+    } else {
+      newQuestion.setQuestionId("false");
+      return newQuestion;
+    }
   }
 
 }
--- web/src/main/resources/static/js/script.js
+++ web/src/main/resources/static/js/script.js
@@ -1,24 +1,82 @@
  $(document).ready(function () {
+     $("#use50_50").click(function () {
+         var answer = $(this).val();
+         var settings = {
+             "async": true,
+             "crossDomain": true,
+             "url": "answer",
+             "method": "POST",
+             "headers": {
+                 "answer": answer
+             }
+         }
 
-    $("#beginGame").click(function () {
-            //var bla = $('#inputButtonPaper').val();
-           // var radioButton = Number($('[name="optradio"]:checked').closest('label').text());
-            var settings = {
+         $.ajax(settings).done(function (response) {
+
+             $('#questionTextarea').text(response.question);
+             if(response.answer_1=="_________"){
+                 $('#answerBtn1').text("-");
+                 $('#answerBtn1').prop('disabled', true);
+             }
+             if(response.answer_2=="_________"){
+                 $('#answerBtn2').text("-");
+                 $('#answerBtn2').prop('disabled', true);
+             }
+             if(response.answer_3=="_________"){
+                 $('#answerBtn3').text("-");
+                 $('#answerBtn3').prop('disabled', true);
+             }
+             if(response.answer_4=="_________"){
+                 $('#answerBtn4').text("-");
+                 $('#answerBtn4').prop('disabled', true);
+             }
+
+             $('#use50_50').prop('disabled', true);
+
+
+         });
+     });
+     $("#beginGame").click(function () {
+           var settings = {
                 "async": true,
                 "crossDomain": true,
                 "url": "startGame",
                 "method": "GET",
                 "headers": {
-                    // "guess": bla,
-                    //"radioButton": radioButton,
                 }
             }
 
             $.ajax(settings).done(function (response) {
 
-               // $('#result').text(response.message);
+               $('#questionTextarea').text(response.question);
+               $('#answerBtn1').text(response.answer_1);
+               $('#answerBtn2').text(response.answer_2);
+               $('#answerBtn3').text(response.answer_3);
+               $('#answerBtn4').text(response.answer_4);
+                $('#use50_50').prop('disabled', false);
             });
         });
+
+
+
+     $("#answerBtn1, #answerBtn2, #answerBtn3, #answerBtn4 ").click(function () {
+         var answer = $(this).val();
+         var settings = {
+             "async": true,
+             "crossDomain": true,
+             "url": "answer",
+             "method": "POST",
+             "headers": {
+                 "answer": answer
+             }
+         }
+
+         $.ajax(settings).done(function (response) {
+
+
+         });
+     });
+
 
 
 
